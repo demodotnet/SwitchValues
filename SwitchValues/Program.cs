d9a0eb2a100ff85488908e466c74047bba0b0046
kumar6646 <sai.penusoft@gmail.com>
2025-07-16 14:19:41 +0530

    Add project files.

---
+++ SwitchValues/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENter a number");
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int b=int.Parse(Console.ReadLine());
+            switch(a>b)
+            {
+                case true:
+                    Console.WriteLine("a is greater than b");
+                    break;
+                    case false:
+                    Console.WriteLine("a is less than b");
+                    break;
+                    
+            }
+        }
+    }
+}
